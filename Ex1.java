2d6b54c02065a1c1789d7dbbac23a6e81e090e2c
cinthia castro <cinthia.castro@7comm.com.br>
2022-02-21 17:00:40 -0300

    exercicios de fixação

---
+++ Ex1.java
@@ -0,0 +1,18 @@
+package POO;
+
+public class Ex1 {
+	
+	public static void main(String[] args) {
+		
+		int y = 32;
+		double x = 10.35784;
+		System.out.println(y);
+		System.out.println(x);
+		System.out.printf("%.2f%n", x);
+		System.out.println("Boa tarde");
+		
+	}
+
+	
+}
+
---
+++ exerciciodefixacao.java
@@ -0,0 +1,25 @@
+package POO;
+
+public class exerciciodefixacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	  String product1 = "Computer";
+	  String product2 = "Office desk";
+	  
+	  int age = 30;
+	  int code = 5290;
+	  char gender = 'F';
+	  
+	  double prince1 = 2100.0;
+	  double prince2 = 650.50;
+	  double measure = 53.234567;
+	  
+	  System.out.println(product1 + " which price is $" + prince1);
+	 
+	  System.out.println(product2 + " which price is $" + prince2);
+
+	}
+
+}
